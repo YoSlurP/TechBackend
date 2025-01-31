772e1544bd07623fc53c9cd3c64360401863985c
YoSlurP <cseh.lalika.gergo@gmail.com>
2025-01-31 20:41:56 +0100

    Free backend

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 import express from "express"
 import cors from "cors"
 import mysql from "mysql2/promise"
+import {infok} from "./Databaseconfig.js"
 
-const con = await mysql.createConnection({
-    host:"localhost",
-    port:3306,
-    database:"techsupport",
-    user:"root",
-    password:""
-});
+const con = await mysql.createConnection(infok);
 
 const app = express();
 app.use(express.json())
 app.use(cors());
 
 async function getuzenet(req,res) {
-    let sql = "select az, uzenete, uaz, saz,cim,date from admin";
+    let sql = "select az, uzenet, uaz, saz,cim,date from admin";
     try{
         const [ json ] = await con.query(sql);
-        resp.send(json);
-    }catch(err){resp.status(500).send({error:err});}
+        res.send(json);
+    }catch(err){res.status(500).send({error:err});}
 }
 async function adduzenet(req,res) {
     if(req.body.cim&&req.body.uzenet&&req.body.uaz&&req.body.saz){
-        try{
-            let sql=`insert into admin set cim="${req.body.cim}", uzenet="${req.body.uzenet}",uaz=${req.body.uaz}, saz=${req.body.saz}, date=${CURRENT_TIMESTAMP}`;
+        // try{
+            let sql=`insert into admin set cim="${req.body.cim}", uzenet="${req.body.uzenet}",uaz=${req.body.uaz}, saz=${req.body.saz}`;
+            console.log(sql)
             const resp = await con.query(sql); 
             res.status(200).send("OK");
-        }catch(err){res.status(500).send({error:err});}
+        // }catch(err){res.status(500).send({error:err});}
     }else res.status(400).send({ error:"Hibás paraméterek!" });
 }
 
