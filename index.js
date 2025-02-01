f4f62dfebd9bdb62eb5bba6279a5eb37e3480f52
YoSlurP <cseh.lalika.gergo@gmail.com>
2025-02-01 17:22:58 +0100

    SQL upd

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 import express from "express"
 import cors from "cors"
 import mysql from "mysql2/promise"
-import {infok} from "./Databaseconfig.js"
 
-const con = await mysql.createConnection(infok);
+const con = await mysql.createConnection({
+    host:process.env.VITE_DB_HOST,
+    port:process.env.VITE_DB_PORT,
+    database:process.env.VITE_DB_DATABASE,
+    user:process.env.VITE_DB_USER,
+    password:process.env.VITE_DB_PASSWORD
+});
 
 const app = express();
 app.use(express.json())
 app.use(cors());
 
 async function getuzenet(req,res) {
-    let sql = "select az, uzenet, uaz, saz,cim,date from admin";
+    let sql = "select az, uzenet, uaz, saz,cim,date from ticket";
     try{
         const [ json ] = await con.query(sql);
         res.send(json);
@@ -18,18 +23,22 @@ async function getuzenet(req,res) {
 }
 async function adduzenet(req,res) {
     if(req.body.cim&&req.body.uzenet&&req.body.uaz&&req.body.saz){
-        // try{
-            let sql=`insert into admin set cim="${req.body.cim}", uzenet="${req.body.uzenet}",uaz=${req.body.uaz}, saz=${req.body.saz}`;
+         try{
+            let sql=`insert into ticket set cim="${req.body.cim}", uzenet="${req.body.uzenet}",uaz="${req.body.uaz}", saz="${req.body.saz},taz=${req.body.taz}"`;
             console.log(sql)
             const resp = await con.query(sql); 
             res.status(200).send("OK");
-        // }catch(err){res.status(500).send({error:err});}
+         }catch(err){res.status(500).send({error:err});}
     }else res.status(400).send({ error:"Hibás paraméterek!" });
+}
+async function getadmin(req,res) {
+    
 }
 
 
 
-app.get("/admin",getuzenet)
+app.get("/uzenetek",getuzenet)
+app.get("/admin",getadmin)
 app.post("/uzenet",adduzenet)
 
 app.listen(88,(error)=> {if(error)console.log(error);else console.log("Server on :88")})
