a4a93aafa52483064ad13736da5b8a1154459b1d
Gero <cseh.lalika.gergo@gmail.com>
2025-02-07 17:24:43 +0100

    Mas megoldas

--- index.js
+++ index.js
@@ -13,22 +13,6 @@ app.use("/admin", adminRouter);
 app.use("/uzenetek", uzenetekRouter);
 app.use(errorHandler);
 
-
-const url = `https://techbackend-app4.onrender.com`; 
-const interval = 30000;
-
-function reloadWebsite() {
-  axios.get(url)
-    .then(response => {
-      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
-    })
-    .catch(error => {
-      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
-    });
-}
-
-setInterval(reloadWebsite, interval);
-
 app.listen(88, (error) => {
   if (error) console.log(error);
   else console.log("Server on :88");
