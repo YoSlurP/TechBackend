54f7ea5d13c97d8b4dfa1058938e92a0e9285769
Gero <cseh.lalika.gergo@gmail.com>
2025-02-07 17:13:34 +0100

    Server Ping

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { adminRouter, uzenetekRouter } from "./routes/index.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
+import axios from "axios";
 dotenv.config();
 
 const app = express();
@@ -11,6 +12,22 @@ app.use(cors());
 app.use("/admin", adminRouter);
 app.use("/uzenetek", uzenetekRouter);
 app.use(errorHandler);
+
+
+const url = `https://techbackend-app4.onrender.com`; 
+const interval = 30000;
+
+function reloadWebsite() {
+  axios.get(url)
+    .then(response => {
+      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+setInterval(reloadWebsite, interval);
 
 app.listen(88, (error) => {
   if (error) console.log(error);
